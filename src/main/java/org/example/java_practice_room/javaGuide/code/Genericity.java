865a30dd71a2fb78d4c2242b518c2baf97e9fb54
ljj <1412415464@qq.com>
2025-11-10 14:41:01 +0800

    《javaGuide》序列化章节学习：泛型章节补充，了解无界通配符以及上下界通配符。

--- src/main/java/org/example/java_practice_room/javaGuide/code/Genericity.java
+++ src/main/java/org/example/java_practice_room/javaGuide/code/Genericity.java
@@ -73,7 +73,14 @@ public class Genericity {
         System.out.println("泛型方法结果如下：" );
         String[] stringArray = { "Hello", "World" };
         printArray(stringArray);
-
-
+        System.out.println("泛型中还有\033[1m类型擦除\033[0m的概念，简单来说就是，泛型是编译器为了减少开发阶段的错误提供给开发者使用的，而在编译过程中虚拟机对此一无所知。" + "\n");
+        System.out.println("泛型中存在一种通配符（？），也叫无界通配符");
+        System.out.println("泛型和无界通配符的区别：");
+        System.out.println("1.T可以用在变量或者常量上，而？不行。");
+        System.out.println("2.T一般用于泛型类或方法中，而？一般是用在泛型方法中。");
+        System.out.println("3.T在编译期会被擦除为限定类型或Object，而通配符只用于捕获具体类型。" + "\n");
+        System.out.println("泛型中的通配符有：上界通配符（extends）和下界通配符（super）");
+        System.out.println("上界通配符：传入类型受到继承的类型限制，即传入的类型必须是继承类型的子类型。");
+        System.out.println("下界通配符：传入类型受到继承的类型限制，即传入的类型必须是继承类型的父类型。");
     }
 }
